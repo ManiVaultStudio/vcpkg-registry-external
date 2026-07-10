572e891114f3db974f8054079bee23e43d79967c
Baldur van Lew <b.van_lew@lumc.nl>
2026-07-10 13:44:03 +0200

    Set build type in the configure step

--- ports/freeimage/portfile.cmake
+++ ports/freeimage/portfile.cmake
@@ -6,7 +6,9 @@ vcpkg_from_github(
   SHA512 "a7e8f289b2123b2f9919b0b2947940665fd373fd348d16a352db46e23c96ae73812dbba7fc7832b95cbadbedb3085f6f44628ae126d24bf30098aa077c1ccf3e"
 )
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+  -DCMAKE_BUILD_TYPE=Release)
 
 
 # Remove /permissive- for OpenEXR and FreeImage
