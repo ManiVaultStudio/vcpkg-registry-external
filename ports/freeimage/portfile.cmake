0f2c38e40be287913e43fa07eb9eeadbd7302c15
Baldur van Lew <b.van_lew@lumc.nl>
2026-07-10 13:19:09 +0200

    Target settings after config

--- ports/freeimage/portfile.cmake
+++ ports/freeimage/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
   SHA512 "a7e8f289b2123b2f9919b0b2947940665fd373fd348d16a352db46e23c96ae73812dbba7fc7832b95cbadbedb3085f6f44628ae126d24bf30098aa077c1ccf3e"
 )
 
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+
 
 # Remove /permissive- for OpenEXR and FreeImage
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
@@ -13,8 +15,6 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
     target_compile_options(FreeImage PRIVATE /permissive)
     target_compile_options(FreeImageLib PRIVATE /permissive) # we don't need the static library, but just in case someone does build it
 endif()
-
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 
 vcpkg_cmake_install()
 
