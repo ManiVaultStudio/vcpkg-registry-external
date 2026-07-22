d8795909156a9dd9150a5350b06bcef756ea0c59
Baldur van Lew <b.van_lew@lumc.nl>
2026-07-22 09:47:11 +0200

    Force hdilib to Release only

--- ports/hdilib/portfile.cmake
+++ ports/hdilib/portfile.cmake
@@ -20,7 +20,7 @@ vcpkg_add_to_path("${CURRENT_HOST_INSTALLED_DIR}/tools/shaderc")
 
 vcpkg_cmake_configure( SOURCE_PATH "${SOURCE_PATH}"
   OPTIONS
-  # -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_BUILD_TYPE=Release
   -DSUPPORT_LIBS_INSTALL=OFF
   -DENABLE_TESTS=OFF
   -DHDILib_BUILD_TESTS=OFF
